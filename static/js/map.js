7e918b9490b3ff8c6fb51bbf3bda0d47ddefa3da
gwanmins <100203726+gwanmins@users.noreply.github.com>
2022-07-01 14:59:23 +0900

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -1,14 +1,73 @@
-const btn=document.getElementById('map');
+// const btn=document.getElementById('map');
 
-function Click(){
-	const img = document.createElement('img'); 
-	img.src = "../static/images/pin1.svg";
-	document.body.appendChild(img);
-	img.style.position='relative';
-	img.style.zIndex="3";
+// function Click(){
+// 	const img = document.createElement('img'); 
+// 	img.src = "../static/images/pin1.svg";
+// 	img.style.position='relative';
+// 	img.style.zIndex="3";
+// 	document.body.appendChild(img);
+// }
+
+// btn.addEventListener('click',Click);
+
+let i = 0;
+
+let click = document.querySelector('.map');
+click.onclick = function(event){
+   var x = event.clientX;
+   var y = event.clientY;
+   const img = document.createElement('img');
+   
+   img.src = "../static/images/pin1.svg";
+   document.body.appendChild(img);
+   
+   img.style.bottom = String(1000-y)+"px";
+
+   img.style.left = String(x-23)+"px";
+   img.style.position='absolute';
+   img.style.zIndex="3";
+   i = i + 1;
+   img.id = "a" + i;
+   console.log("a"+i);
 }
 
-btn.addEventListener('click',Click);
+// if(i>=5){
+	let firstImage = document.getElementById("a1");
+	let secondImage = document.getElementById("a2");
+	firstImage.addEventListener("click", () => {
+		firstImage.remove();
+	});
+	secondImage.addEventListener("click", () => {
+		secondImage.remove();
+	});
+	console.log("삭제됐습니다");
+// }
+// if(i==5){
+// 	for(j=1; j<6; j++){
+// 		let selectImage=document.getElementById('"a"+j');
+// 		console.log(selectImage);
+// 	}
+	// selectImage.remove();
+	// if (click.onclick== "a"+j){
+	// 	("a"+j).remove();
+	// }
+
+// }
+
+// function Click(e){
+// 	const evt = window.event ? window.event : e;  // 이벤트 받기 (파폭, IE 처리)
+ 
+// 	const img = document.createElement('img');    // 이미지 객채 생성
+	
+
+// 	img.src = "../static/images/pin1.svg"// 이미지 경로 설정 (랜덤)
+// 	img.style.position = 'relative';
+// 	img.style.zIndex=2;
+// 	img.style.top = evt.clientY + document.body.scrollTop;
+// 	img.style.left = evt.clientX + document.body.scrollLeft;
+// 	document.body.appendChild(img);
+// }
+// document.onclick = Click;
 
 
 
@@ -65,13 +124,3 @@ btn.addEventListener('click',Click);
 // let img= document.createElement("img");
 // img.setAttribute("src", "../images/pin\ 1.svg");
 
-
-
-// function Init()
-// {
-//   document.onmousedown=function()
-// }
-
-// if (event.button != "1") return;
-// let x= event.x;
-// let y=event.y;
