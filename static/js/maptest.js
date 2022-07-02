d3575b9f3b3d252cb1f8275701ae2d1acb2daba8
gwanmins <rhkdals0916@naver.com>
2022-07-02 20:57:25 +0900

    a..

--- static/js/maptest.js
+++ static/js/maptest.js
@@ -1,10 +1,14 @@
 let i = 0;
+let buttons = document.querySelectorAll('button');
+let input=document.getElementById('search')
 
-document.onmousedown = function (event) {
+document.onmousedown = function Click(event) {
     if (event.button == 0) {
+    
+
       document.title = event.clientX + ", " + event.clientY
       let img = document.createElement("img");
-      img.src = "../static/images/오늘이 프로필.svg"; 
+      img.src = "../static/images/오늘이 핀.svg"; 
   
       img.onload = function (event) {
         img.style.left = img.offsetLeft - img.width / 2 + "px";
@@ -14,16 +18,18 @@ document.onmousedown = function (event) {
       img.style.left = event.clientX + "px";
       img.style.top = event.clientY + "px";
       img.style.position = "absolute"
-  
+
       document.body.appendChild(img);
       i = i + 1;
-  	  console.log("a"+i);
+      img.id = "a" + i;
+      img.className='pin';
+      console.log(img.id);
     }
   
     else if (event.button==1){
         document.title = event.clientX + ", " + event.clientY
         let img = document.createElement("img");
-        img.src = "../static/images/내일이 프로필.svg"; 
+        img.src = "../static/images/내일이 핀.svg"; 
     
         img.onload = function (event) {
           img.style.left = img.offsetLeft - img.width / 2 + "px";
@@ -48,6 +54,93 @@ document.onkeydown=function onKeyUp(e){
     }
 };
 
+const pin = document.querySelector(".pin");
+function pinClick() {
+  pin.classList.toggle("clicked");
+  //clicked라는 css로 왔다갔다
+}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const pin = document.querySelector(".pin");
+// const db1=document.querySelector('body1');
+// function dblclick(){
+// //   if (event.button == 0) { 더블클릭
+//      document.title = event.clientX + ", " + event.clientY
+//     let img = document.createElement("img");
+//     img.src = "../static/images/pin 1.svg"; 
+
+//     img.onload = function (event) {
+//       img.style.left = img.offsetLeft - img.width / 2 + "px";
+//       img.style.top = img.offsetTop - img.height / 2 + "px";
+//     }
+
+//     img.style.left = event.clientX + "px";
+//     img.style.top = event.clientY + "px";
+//     img.style.position = "absolute"
+
+//     document.body.appendChild(img);
+//     i = i + 1;
+//     img.id = "a" + i;
+//     img.className='pin';
+//     console.log(img.id);
+  
+//   }
+
+//   else if (event.button==1){
+//     document.title = event.clientX + ", " + event.clientY
+//     let img = document.createElement("img");
+//     img.src = "../static/images/pin 2.svg"; 
+
+//     img.onload = function (event) {
+//       img.style.left = img.offsetLeft - img.width / 2 + "px";
+//       img.style.top = img.offsetTop - img.height / 2 + "px";
+//     }
+
+//     img.style.left = event.clientX + "px";
+//     img.style.top = event.clientY + "px";
+//         img.style.position = "absolute"
+    
+//     document.body.appendChild(img);
+//     i = i + 1;
+//     console.log("a"+i);
+//   }
+// };
+
+
+
+
+
+// function select(){
+//   for(k=1; k<11; k++){
+//      let tp=document.getElementById('"a"+i');
+//      tp.classList.toggle("clicked");    
+//   }
+// }
 
 
