eb951ab5e73b5989f133f2ff0876b1a05917a749
math0917 <math0917@naver.com>
2022-07-06 17:27:29 +0900

    mapafter2

--- static/js/mapAfter2.js
+++ static/js/mapAfter2.js
@@ -5,10 +5,10 @@ window.onload = function(){
     context.lineWidth = 8;
     context.beginPath();
 
-    context.moveTo(87,40);
-	context.lineTo(310,137);
-    context.moveTo(87,42);
-    context.lineTo(220,200);
+    context.moveTo(75,35);
+	  context.lineTo(270,115);
+    context.moveTo(75,35);
+    context.lineTo(163,140);
     context.stroke();
 
     
